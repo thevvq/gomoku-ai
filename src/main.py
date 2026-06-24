84aa96f4c4db7223264c917514fd9d99f287dc75
SherlockTran <tranhuuthanhdat107@gmail.com>
2026-06-24 23:18:04 +0700

    Change server port from 7890 to 7891

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import uvicorn
     print("=======================================")
     print("Starting Gomoku Web Server with FastAPI...")
-    print("Open http://localhost:7890 in your browser.")
+    print("Open http://localhost:7891 in your browser.")
     print("=======================================")
     # Run the FastAPI app located in src.server
-    uvicorn.run("server:app", host="0.0.0.0", port=7890, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=7891, reload=True)
